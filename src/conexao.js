ab21c4865ab1fbb2e3cde47fb8e1922780181637
Guilherme <guilhherme10@outlook.com>
2023-10-04 19:43:58 -0300

    teste

--- src/conexao.js
+++ src/conexao.js
@@ -1,14 +1,14 @@
 require(`dotenv`).config()
 
-
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host : process.env.HOST,
-      port : process.env.PORT_DATABASE,
-      user :  process.env.USER,
-      password : process.env.PASSWORD,
-      database : process.env.DATA_BASE
+      host : process.env.DB_HOST,
+      port : process.env.DB_PORT,
+      user :  process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_NAME,
+      ssl: {rejectUnauthorized: false}
     }
   });
   module.exports = knex
